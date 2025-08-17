8955fbf3da784d233ee4bd143771b35de87f42e9
KepaMax <98890924+KepaMax@users.noreply.github.com>
2025-08-17 15:41:22 +0400

    feat: Enhance TransactionShowDTO and ExcelUsagePackage with new properties and update repository interfaces for improved functionality

--- Application/Models/DTOs/TransactionShowDTO.cs
+++ Application/Models/DTOs/TransactionShowDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Models.DTOs.User;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace Application.Models.DTOs
 {
-    internal class TransactionShowDTO
+    public class TransactionShowDTO
     {
+        public double Amount { get; set; }
+        public string OrderId { get; set; }
+        public string? UserId { get; set; }
+        public UserShowDTO? User { get; set; }
+        public string MaskedPan { get; set; }
+        public string ApprovalCode { get; set; }
+        public string PaymentUrl { get; set; }
+        public string SeesionId { get; set; }
+        public string? Description { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? PaidAt { get; set; }
+        public Guid ExcelUsagePackageId { get; set; }
+        public virtual ExcelUsagePackage ExcelUsagePackage { get; set; }
     }
 }
--- Domain/Entities/ExcelUsagePackage.cs
+++ Domain/Entities/ExcelUsagePackage.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entities
+﻿namespace Domain.Entities
 {
-    internal class ExcelUsagePackage
+    public class ExcelUsagePackage : BaseEntity
     {
+        public int RowsCount { get; set; }
+        public int UsedRowsCount { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+        public bool IsActive { get; set; } = true;
+        public Guid TransactionId { get; set; }
+        public virtual Transaction Transaction { get; set; }
     }
 }
